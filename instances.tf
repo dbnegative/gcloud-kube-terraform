3346aa18258332fb51d60ad3c782fefd3c52fc7e
Jason Witting <jason.witting@MacBook-Pro-4.local>
2016-07-11 16:14:33 +0100

    fixed typeo

--- instances.tf
+++ instances.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "etcd-cluster" {
   name         = "etcd${count.index}"
   machine_type = "${var.instance-type}"
-  zone         = "${var.region}${element(var.az, count.index)}"
+  zone         = "${var.region}-${element(var.az, count.index)}"
   disk {
       image = "${var.disk["image"]}"
       size = 200
@@ -21,7 +21,7 @@ resource "google_compute_instance" "etcd-cluster" {
 resource "google_compute_instance" "controller-cluster" {
   name         = "controller${count.index}"
   machine_type = "${var.instance-type}"
-  zone         = "${var.region}${element(var.az, count.index)}"
+  zone         = "${var.region}-${element(var.az, count.index)}"
   disk {
       image = "${var.disk["image"]}"
       size = 200
@@ -41,7 +41,7 @@ resource "google_compute_instance" "controller-cluster" {
 resource "google_compute_instance" "worker-cluster" {
   name         = "worker${count.index}"
   machine_type = "${var.instance-type}"
-  zone         = "${var.region}${element(var.az, count.index)}"
+  zone         = "${var.region}-${element(var.az, count.index)}"
   disk {
       image = "${var.disk["image"]}"
       size = 200
