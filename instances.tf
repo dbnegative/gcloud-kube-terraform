97460c5d0e50489d2bc62eae6d6c468c7d654fe2
Jason Witting <jason.witting@MacBook-Pro-4.local>
2016-07-14 17:36:01 +0100

    migrate controller and worker prov to ansible

--- instances.tf
+++ instances.tf
@@ -21,34 +21,6 @@ resource "google_compute_instance" "etcd-cluster" {
    service_account {
     scopes = ["compute-ro", "storage-ro"]
   }
-/*  
-  provisioner "file" {
-        source = "scripts/etcd_configure.sh"
-        destination = "~/etcd_configure.sh"
-        
-        connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-      }
-    }
-  
-  provisioner "remote-exec" {
-      connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-    }
-      
-      inline = [
-          "gsutil cp gs://kube-ssl-certs/kubernetes.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/kubernetes-key.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/ca.pem  ~/.", 
-          "chmod 755 ~/etcd_configure.sh",
-          "~/etcd_configure.sh"
-      ]
-  }
-  */
 }
 
 #---------------- CONTROLLER ----------------------------------------
@@ -75,33 +47,6 @@ resource "google_compute_instance" "controller-cluster" {
   service_account {
     scopes = ["compute-ro", "storage-ro"]
   }
-  
-  provisioner "file" {
-        source = "scripts/kubectrl_configure.sh"
-        destination = "~/kubectrl_configure.sh"
-        
-        connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-      }
-    }
-
-  provisioner "remote-exec" {
-      connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-    }
-      
-      inline = [
-          "gsutil cp gs://kube-ssl-certs/kubernetes.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/kubernetes-key.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/ca.pem  ~/.",
-          "chmod 755 ~/kubectrl_configure.sh",
-          "~/kubectrl_configure.sh"
-      ]
-  }
 }
 #----------------- WORKER ------------------------------------
 #
@@ -126,32 +71,5 @@ resource "google_compute_instance" "worker-cluster" {
   
   service_account {
     scopes = ["compute-ro", "storage-ro"]
-  }
-  
- provisioner "file" {
-        source = "scripts/kubeworker_configure.sh"
-        destination = "~/kubeworker_configure.sh"
-        
-        connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-      }
-    }
-
-  provisioner "remote-exec" {
-      connection {
-        type = "ssh"
-        user = "shortjay"
-        private_key = "~/.ssh/google_compute_1"
-    }
-      
-      inline = [
-          "gsutil cp gs://kube-ssl-certs/kubernetes.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/kubernetes-key.pem  ~/.",
-          "gsutil cp gs://kube-ssl-certs/ca.pem  ~/.",
-          "chmod 755 ~/kubeworker_configure.sh",
-          "~/kubeworker_configure.sh"
-      ]
   }
 }
